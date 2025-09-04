5695002d6e328a2ab4ea71f8597a3720e125d2e3
nathanpee2006 <nathanpee2006@gmail.com>
2025-09-04 14:18:00 +1200

    Add header and footer to all pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,19 +1,23 @@
 import { ThemeProvider } from "./components/theme-provider";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Projects from "./sections/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </ThemeProvider>
   );
 }
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,23 +1,17 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Hero from "../sections/Hero";
 import Background from "../sections/Background";
 import Projects from "../sections/Projects";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <div className="mx-auto max-w-3xl flex flex-col px-10">
-        <main>
-          <article className="mt-8 flex flex-col gap-16 pb-16">
-            <Hero />
-            <Background />
-            <Projects />
-          </article>
-        </main>
-      </div>
-      <Footer />
-    </>
+    <div className="mx-auto max-w-3xl flex flex-col px-10">
+      <main>
+        <article className="mt-8 flex flex-col gap-16 pb-16">
+          <Hero />
+          <Background />
+          <Projects />
+        </article>
+      </main>
+    </div>
   );
 }
